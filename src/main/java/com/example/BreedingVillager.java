0728948bac8885ff62e67b2fe3724141462fc2cb
Wings-Ranger <lachlan.gaaschs@gmail.com>
2026-04-16 16:46:06 +1000

    setting up the workspace

--- src/main/java/com/example/BreedingVillager.java
+++ src/main/java/com/example/BreedingVillager.java
@@ -5,8 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class BreedingVillager implements ModInitializer {
+	public static final String MOD_ID = "breedingvillager";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
